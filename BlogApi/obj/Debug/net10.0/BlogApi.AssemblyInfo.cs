041eee594e214bddb672843ff07ae123c7255a6d
gecsel <gecsel@kkszki.hu>
2026-09-16 08:25:56 +0200

    UpdateBlogger added

--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ea10122f634831e6501908e9ff5f3355feda3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4259533b7010b3365899d7f1080b7bb7de02f1cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
